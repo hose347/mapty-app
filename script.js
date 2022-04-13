94308e315b36d1c488f8a41a3178ea5d030d9b52
hose347 <hosse.css@gmail.com>
2022-04-13 18:56:10 +0200

    render workout + hideform + setting description

--- script.js
+++ script.js
@@ -23,19 +23,23 @@ class Workout {
     this.duration = duration;
   }
 
-  setDescription() {
+  _setDescription() {
     // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    this.description = `${this.type[0].toUppercase()}`;
+    // Zapytaj sie damiana o skad description bierze tego type ? jak wyglada ten chain do tego type////actualizacja/// dobra chyba wiem, ale sie dopytaj
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
   }
 }
 
 class Running extends Workout {
+  type = 'running';
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPace();
+    this._setDescription();
   }
 
   calcPace() {
@@ -46,10 +50,12 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = 'cycling';
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
     this.calcSpeed();
+    this._setDescription();
   }
 
   calcSpeed() {
@@ -112,6 +118,18 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    // Empty inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
+  }
+
   _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
@@ -164,12 +182,16 @@ class App {
       workout = null;
       return;
     }
-    this._renderWorkout(workout);
+
     // Add new object to workout array
     this.#workout.push(workout);
     console.log(workout);
     // Render workout on map as marker
+    this._renderWorkout(workout);
+    // Render workout on list
     this._renderWorkoutMarker(workout, type);
+    // Hide form + Clear input fields
+    this._hideForm();
   }
 
   _renderWorkoutMarker(workout, type) {
@@ -187,23 +209,19 @@ class App {
           className: `${type}-popup`,
         })
       )
-      .setPopupContent(`${workout.distance}`)
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description} `
+      )
       .openPopup();
-    // Hide form + Clear input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
   }
 
   _renderWorkout(workout) {
-    const html = `
+    let html = `
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
-    <h2 class="workout__title">Running on April 14</h2>
+    <h2 class="workout__title">${workout.description}</h2>
     <div class="workout__details">
       <span class="workout__icon">${
-        workout.name === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+        workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
       }</span>
       <span class="workout__value">${workout.distance}</span>
       <span class="workout__unit">km</span>
@@ -213,7 +231,36 @@ class App {
       <span class="workout__value">${workout.duration}</span>
       <span class="workout__unit">min</span>
     </div>
+    
 `;
+
+    if (workout.type === 'running')
+      html += ` <div class="workout__details">
+  <span class="workout__icon">⚡️</span>
+  <span class="workout__value">${workout.pace.toFixed(1)}</span>
+  <span class="workout__unit">min/km</span>
+</div>
+<div class="workout__details">
+  <span class="workout__icon">🦶🏼</span>
+  <span class="workout__value">${workout.cadence}</span>
+  <span class="workout__unit">spm</span>
+</div>
+</li>`;
+
+    if (workout.type === 'cycling')
+      html += `<div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li> `;
+    //powoduje to umieszcznie elementu jakos siblinga do elementu form
+    form.insertAdjacentHTML('afterend', html);
   }
 }
 
